1595bebd918c150b3fff06e3d56d9b03c31a0db0
Ameen Al-Azzawi <45686881+ameen-mcmxc@users.noreply.github.com>
2023-05-27 10:52:11 +0200

    Delete source-port-exhaustion.sh

--- scripts/source-port-exhaustion.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-/root/dns64perfpp-a/dns64perf++v4 192.0.2.2 53 0.0.0.0/5 60000 1 1 60000 400000 0.1
-
-# Syntax
-# Usage: dns64perf++ <server> <port> <subnet>
-#  <number of requests> <burst size> <number of threads> <number of ports per thread> <delay between bursts in ns> <timeout in s>
